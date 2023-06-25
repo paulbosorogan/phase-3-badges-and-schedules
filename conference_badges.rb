25075c92a8b4ad2aed29416e70c53c5af6295559
Paul Bosorogan <bosorogan.paul@gmail.com>
2023-06-25 10:35:31 -0400

    commit tree 8a7a25cfa58755e46fe5d06de8f8097403f3d03e

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker name
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator array_of_names
+    array_of_names.map do |name| 
+     "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms array_of_names
+    array_of_names.map.with_index(1) do |name, room_num|
+        "Hello, #{name}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer array_of_names
+    batch_badge_creator(array_of_names).each do |badge|
+        puts badge 
+    end
+
+    assign_rooms(array_of_names).each do |assign|
+        puts assign
+    end
+end
